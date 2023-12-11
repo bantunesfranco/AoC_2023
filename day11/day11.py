5e05c0a91395b5ef5bf28604384589da9515c343
Bruno Franco <94724432+bantunesfranco@users.noreply.github.com>
2023-12-11 20:29:49 +0000

    day11

--- day11/day11.py
+++ day11/day11.py
@@ -1,6 +1,7 @@
-with open("input.txt") as f:
+with open("test.txt") as f:
 	lines = f.read().splitlines()
 
+### first try
 def insert (src, s, pos):
 	return src[:pos] + s + src[pos:]
 
@@ -58,12 +59,12 @@ galaxies = set()
 addy = 0
 for y, line in enumerate(space):
 	if all(c == "1" for c in line):
-		addy += (10**6 - 2)
+		addy += (1000000 - 2)
 	addx = 0
 	for x, char in enumerate(line):
 		col = [line[x] for line in space]
 		if all(c == "1" for c in col):
-			addx += (10**6 - 2)
+			addx += (1000000- 2)
 		if char == "#":
 			galaxies.add((x+addx, y+addy))
 
@@ -73,4 +74,25 @@ for i, (x1, y1) in enumerate(galaxies):
 	for (x2, y2) in galaxies[i+1:]:
 		res.append(abs(x2-x1) + abs(y2-y1))
 
-print(f"part 2 output = {sum(res)}")
+print(f"part 2 output = {sum(res)}\n")
+
+### new solution
+def space_walk(lines, factor) -> int:
+	empty_lines = [y for y, line in enumerate(lines) if all(c == "." for c in line)]
+	empty_cols = [x for x, _ in enumerate(lines[0]) if all(line[x] == "." for line in lines)]
+
+	galaxies = [(y, x) for y, line in enumerate(lines) for x, c in enumerate(line) if c == "#"]
+	sum = 0
+	for i , (x1, y1) in enumerate(galaxies):
+		for (x2, y2) in galaxies[i+1:]:
+			sum += abs(x2-x1) + abs(y2-y1)
+			for p in empty_cols:
+				if min(y1, y2) <= p <= max(y1, y2):
+					sum += (factor - 1)
+			for p in empty_lines:
+				if min(x1, x2) <= p <= max(x1, x2):
+					sum += (factor - 1)
+	return sum
+
+print(f"part 1 output = {space_walk(lines, 2)}")
+print(f"part 2 output = {space_walk(lines, 1000000)}")
