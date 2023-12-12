b76f61c3328d08f70282cc599bef6449544df866
Bruno Franco <94724432+bantunesfranco@users.noreply.github.com>
2023-12-12 17:41:55 +0000

    day2

--- day2/day2.py
+++ day2/day2.py
@@ -1,26 +1,7 @@
 with open("input.txt") as f:
 	lines = f.readlines()
 	
-## Part 1
 games = 0 
-
-for i, line in enumerate(lines, start=1):
-	line = line.split(":")[1].strip().split(";")
-	cubes = {"red":0, "green":0, "blue":0}
-	for set in line:
-		pulls = set.split(",")
-		for item in pulls:
-			item = item.strip(" ")
-			value, key = item.split(" ")
-			if int(value) > cubes[key]:
-				cubes[key] = int(value)
-	if cubes["red"] <= 12 and cubes["green"] <= 13 and cubes["blue"] <= 14:
-		games += i
-
-print(f"Part 1 output = {games}")
-
-## Part 2
-
 total_power = 0 
 
 for i, line in enumerate(lines, start=1):
@@ -33,6 +14,13 @@ for i, line in enumerate(lines, start=1):
 			value, key = item.split(" ")
 			if int(value) > cubes[key]:
 				cubes[key] = int(value)
+
+## Part 1
+	if cubes["red"] <= 12 and cubes["green"] <= 13 and cubes["blue"] <= 14:
+		games += i
+
+## Part 2
 	total_power += (cubes["red"] * cubes["green"] * cubes["blue"])
 
+print(f"Part 1 output = {games}")
 print(f"Part 1 output = {total_power}")
